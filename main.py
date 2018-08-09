9dc4f316232bcd48037808a0e7579651ca381c72
Minkyung <mnk918p@gmail.com>
2018-08-09 11:28:43 +0900

    add slackbot

--- main.py
+++ main.py
@@ -3,16 +3,32 @@ from rtmbot.core import Plugin
 
 import secret
 
+def answer(text):
+    if "민경" in text:
+        reply = "불렀어?"
+    elif "주사위" == text:
+        reply = str(random.randint(1,6))
+    else:
+        reply = None
+    return reply
+
+    # if "똥글" in data["text"]:
+    #     self.outputs.append([data["channel"], "세계최강귀요미가 맞다."])
+    # elif "주사위" == data["text"]:
+    #     die = str(random.randint(1, 6))
+    #     self.outputs.append([data["channel"], die])
+    # elif "채원" in data["text"]:
+    #     self.outputs.append([data["channel"], "바보ㅡ3ㅡ"])
+    # else:
+    #     pass
+
 
 class HelloPlugin(Plugin):
     def process_message(self, data):
-        if "애란" in data["text"]:
-            self.outputs.append([data["channel"], "불렀어?"])
-        elif "주사위" == data["text"]:
-            die = str(random.randint(1, 6))
-            self.outputs.append([data["channel"], die])
-        else:
-            pass
+        reply = answer(data["text"])
+        if reply is not None:
+            self.outputs.append([data["channel"],reply])
+
 
 
 config = {
